68f8ffdae2e38030da5c99165ae835c060c25b9a
kailau1 <71905911+kailau1@users.noreply.github.com>
2021-08-20 14:05:25 +0100

    initialising repo

---
+++ src/components/Products/products.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Grid from '@material-ui/core'
+
+const products = [
+    { id:2, name: 'iPhone 11', description: 'Apple iPhone 11'},
+    { id:3, name: 'Galaxy s10', description: 'Samsung Galaxy s10'},
+
+
+];
+
+
+const Products = () => {
+    <main>
+        <Grid container justify ="center" spacing={4}>
+            {products.map((product) => (
+            <Grid itme key={product.id} xs={12} sm={6} md={4} lg={3}>
+                <Product />
+            </Grid>
+            ))}
+        </Grid>
+
+    </main>
+}
+
+
+export default products; 
